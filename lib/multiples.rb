13a130ce02eca5cb689fd75180b00149e4fea82b
osgood gunawan <ogunawan@ucsd.edu>
2020-06-24 20:45:14 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,21 @@
-# Enter your procedural solution here!
+
+def collect_multiples(limit)
+    sum = []
+    i = 3
+    while i < limit do
+        if i % 3 == 0 || i % 5 ==0
+            sum << i
+        end
+        i +=1
+    end
+    sum
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum
+end 
+
+
+
+
+
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,24 @@
-# Enter your object-oriented solution here!
+class Multiples 
+attr_reader :limit
+    def initialize (limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        sum = []
+        i = 3
+        while i < limit do
+            if i % 3 == 0 || i % 5 == 0
+                sum << i
+            end
+            i +=1
+        end
+        sum
+    end
+    
+    def sum_multiples
+        collect_multiples.sum
+    end 
+
+
+end 
